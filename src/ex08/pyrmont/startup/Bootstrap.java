830f2b658d744fb126b8b6de805428bdd206f89f
lugew <xia_Gitlab5762566>
2019-01-09 10:54:14 +0800

    ex08

---
+++ src/ex08/pyrmont/startup/Bootstrap.java
@@ -0,0 +1,62 @@
+package ex08.pyrmont.startup;
+
+import example.ex08.pyrmont.core.SimpleContextConfig;
+import example.ex08.pyrmont.core.SimpleWrapper;
+import org.apache.catalina.*;
+import org.apache.catalina.connector.http.HttpConnector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.loader.WebappClassLoader;
+import org.apache.catalina.loader.WebappLoader;
+import org.apache.naming.resources.ProxyDirContext;
+
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        System.setProperty("catalina.base", System.getProperty("user.dir"));
+        Connector connector = new HttpConnector();
+        Wrapper wrapper1 = new SimpleWrapper();
+        wrapper1.setName("Primitive");
+        wrapper1.setServletClass("PrimitiveServlet");
+        Wrapper wrapper2 = new SimpleWrapper();
+        wrapper2.setName("Modern");
+        wrapper2.setServletClass("ModernServlet");
+
+        Context context = new StandardContext();
+
+        context.setPath("/myApp");
+        context.setDocBase("myApp");
+
+        context.addChild(wrapper1);
+        context.addChild(wrapper2);
+
+        context.addServletMapping("/Primitive", "Primitive");
+        context.addServletMapping("/Modern", "Modern");
+
+        LifecycleListener listener = new SimpleContextConfig();
+        ((Lifecycle) context).addLifecycleListener(listener);
+
+        Loader loader = new WebappLoader();
+        context.setLoader(loader);
+
+        connector.setContainer(context);
+
+        try {
+            connector.initialize();
+            ((Lifecycle) connector).start();
+            ((Lifecycle) context).start();
+            // now we want to know some details about WebappLoader
+            WebappClassLoader classLoader = (WebappClassLoader) loader.getClassLoader();
+            System.out.println("Resources' docBase: " + ((ProxyDirContext) classLoader.getResources()).getDocBase());
+            String[] repositories = classLoader.findRepositories();
+            for (int i = 0; i < repositories.length; i++) {
+                System.out.println("  repository: " + repositories[i]);
+            }
+
+            // make the application wait until we press a key.
+            System.in.read();
+            ((Lifecycle) context).stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
